c64300321e441e50be32a4031c4de80d176bfdfd
BaronMbakwe <baronmbakwe34@gmail.com>
2021-12-10 13:42:34 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){return cats.splice(3,0,name)}
+function destructivelyPrependCat(name){return cats.splice(0,0,name)}
+function destructivelyRemoveLastCat(name){return cats.splice(2,1)}
+function destructivelyRemoveFirstCat(name){return cats.splice(0,1)}
+function appendCat(name){return [...cats,name]}
+function prependCat(name){return[name,...cats]}
+function removeLastCat(name){return cats.slice(0,2)}
+function removeFirstCat(name){return cats.slice(1)}
